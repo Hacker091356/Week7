71e39b3e69db9033d06f380980f6783768cd8def
Hacker091356 <ninno2812@gmail.com>
2024-03-20 14:50:38 -0400

    Create numberSum.java

---
+++ numberSum.java
@@ -0,0 +1,33 @@
+//Importing the scanner
+import java.util.Scanner;
+
+public class numberSum {
+    public static void main(String[] args){
+        //Initializing scanner
+        Scanner userInput = new Scanner(System.in);
+        //Asking user for input
+        System.out.print("Enter a number: ");
+        //Storing user input in variable
+        int num = userInput.nextInt();
+        //Closing the scanner to avoid wasteage of resources
+        userInput.close();
+        int startNum = 1;
+        //Creating variable for sum
+        int sum = ((num * (num + 1))/2);
+        //Making the while loop
+        System.out.println(startNum);
+        while(startNum < num)
+        {
+            startNum++; 
+            System.out.println(startNum);
+            if (startNum == num){
+            System.out.print("The sum is: " + sum);
+            break;
+            
+            }
+        }
+        
+            
+             
+    }
+}
