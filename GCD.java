7687cd28fefb8502c503f67278431d2f8090c303
Hacker091356 <ninno2812@gmail.com>
2024-03-20 14:59:12 -0400

    Create GCD.java

---
+++ GCD.java
@@ -0,0 +1,14 @@
+//Importing scanner
+import java.util.Scanner;
+public class GCD {
+    public static void main(String[] Strings){
+        //Initializing scanner
+        Scanner userInput = new Scanner(System.in);
+        //Asking the user for inputs
+        System.out.print("Enter a non-negative integer: ");
+        int num1 = userInput.nextInt();
+        System.out.print("\nEnter a second non-negative integer: ");
+        int num2 = userInput.nextInt();
+    }
+    
+}
