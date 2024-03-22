a41d7c1521fee206ff31326d9175dadb48f72c5a
Hacker091356 <ninno2812@gmail.com>
2024-03-22 17:33:45 -0400

    Update investment.java

--- investment.java
+++ investment.java
@@ -1,7 +1,12 @@
+//Imports
+import java.lang.Math;
 
 public class investment {
     public static void main(String[] args){
-        double interest = 1.075;
-        
+        double interest = 1.075; 
+        double years = (Math.log10(2))/(Math.log10(interest));
+        System.out.print("Your principal of $2500 will take " + years + " years to be valued $5000 at an interest rate of 1.075% compounded yearly");
+
+
     }
 }
